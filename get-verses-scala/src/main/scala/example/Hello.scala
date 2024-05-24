f7b5be1b1518e598bdd390737837cae87ee60ca4
Emmanuel Nkrumah-Sarpong <nkrumahthis@gmail.com>
2024-05-24 23:55:16 +0000

    read the bible

--- get-verses-scala/src/main/scala/example/Hello.scala
+++ get-verses-scala/src/main/scala/example/Hello.scala
@@ -1,7 +1,14 @@
 package example
 
+import scala.io.Source
+
 object Hello extends Greeting with App {
   println(greeting)
+  val filename = "data/en_bbe.json"
+  val filesource = Source.fromFile(filename)
+  val bibleString = filesource.mkString("")
+  println(bibleString)
+  filesource.close()
 }
 
 trait Greeting {
