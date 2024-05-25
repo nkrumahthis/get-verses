414773e9cfc537870a01cfe7d1756f5694277b90
Emmanuel Nkrumah-Sarpong <nkrumahthis@gmail.com>
2024-05-25 20:10:38 +0000

    can get verse from book, chapter and verse

--- get-verses-scala/src/main/scala/getverses/GetVerse.scala
+++ get-verses-scala/src/main/scala/getverses/GetVerse.scala
@@ -1,15 +1,23 @@
 package getverses
 
 import scala.io.Source
+import scala.io.StdIn.readLine
 
 object GetVerse extends App {
   val filename = "data/en_bbe.json"
   val fileSource = Source.fromFile(filename)
   val bibleString = fileSource.mkString.replace("﻿", "")
   val bibleObject = ujson.read(bibleString)
+
+  println("Enter the book, chapter and verse to show")
+
+  private val bookInput = readLine("book > ")
+  private val chapterInput = readLine("chapter > ")
+  private val verseInput = readLine("verse > ")
+
   bibleObject.arr.foreach(book => {
-    if(book("name").str == "Genesis") {
-      val verse = book("chapters").arr(0)(1)
+    if(book("name").str == bookInput) {
+      val verse = book("chapters").arr(chapterInput.toInt - 1)(verseInput.toInt - 1)
       println(verse)
     }
   })
