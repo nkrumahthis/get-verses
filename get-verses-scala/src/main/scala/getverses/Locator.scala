23a0941f27a19cf527066c75929d5bb7ce6e485a
Emmanuel Nkrumah-Sarpong <nkrumahthis@gmail.com>
2024-05-29 23:20:30 +0000

    check if verse actually exists

--- get-verses-scala/src/main/scala/getverses/Locator.scala
+++ get-verses-scala/src/main/scala/getverses/Locator.scala
@@ -6,7 +6,17 @@ class Locator(bibleObject: Value.Value) {
   def locate(bookName: String, chapterNumber: String, verseNumber: String): Option[String] = {
     bibleObject.arr.find(book => book("name").str.toUpperCase == bookName.toUpperCase) match {
       case Some(book) =>
-        Some(book("chapters").arr(chapterNumber.toInt - 1)(verseNumber.toInt - 1).str)
+        val chapters = book("chapters").arr
+        if(chapters.isDefinedAt(chapterNumber.toInt - 1)) {
+          val verses = chapters(chapterNumber.toInt - 1).arr
+          if(verses.isDefinedAt(verseNumber.toInt - 1)) {
+            Some(verses(verseNumber.toInt - 1).str)
+          } else {
+            None
+          }
+        } else {
+          None
+        }
       case None => None
     }
   }
