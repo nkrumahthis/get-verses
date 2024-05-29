446e1c20d5b02b63fbaa13031b33fa2136378696
Emmanuel Nkrumah-Sarpong <nkrumahthis@gmail.com>
2024-05-29 23:45:13 +0000

    get book by abbreviation

--- get-verses-scala/src/main/scala/getverses/Locator.scala
+++ get-verses-scala/src/main/scala/getverses/Locator.scala
@@ -4,12 +4,12 @@ import ujson.Value
 
 class Locator(bibleObject: Value.Value) {
   def locate(bookName: String, chapterNumber: String, verseNumber: String): Option[String] = {
-    bibleObject.arr.find(book => book("name").str.toUpperCase == bookName.toUpperCase) match {
+    findBook(bookName) match {
       case Some(book) =>
         val chapters = book("chapters").arr
-        if(chapters.isDefinedAt(chapterNumber.toInt - 1)) {
+        if (chapters.isDefinedAt(chapterNumber.toInt - 1)) {
           val verses = chapters(chapterNumber.toInt - 1).arr
-          if(verses.isDefinedAt(verseNumber.toInt - 1)) {
+          if (verses.isDefinedAt(verseNumber.toInt - 1)) {
             Some(verses(verseNumber.toInt - 1).str)
           } else {
             None
@@ -17,7 +17,10 @@ class Locator(bibleObject: Value.Value) {
         } else {
           None
         }
-      case None => None
     }
   }
+
+  private def findBook(bookName: String): Option[Value] =
+    bibleObject.arr.find(book => book("name").str.equalsIgnoreCase(bookName)
+      || book("abbrev").str.equalsIgnoreCase(bookName))
 }
