d6ca63bb5b6e86eebb5036a4708067f01a2502ba
Emmanuel Nkrumah-Sarpong <nkrumahthis@gmail.com>
2024-05-29 22:53:07 +0000

    error handling for input

--- get-verses-scala/src/main/scala/getverses/OneStepLocator.scala
+++ get-verses-scala/src/main/scala/getverses/OneStepLocator.scala
@@ -6,18 +6,27 @@ import scala.io.StdIn.readLine
 
 class OneStepLocator(bibleObject: Value.Value) extends Locator(bibleObject) {
   def executor(): Unit = {
-    println("Enter book chapter:verse")
+    println("Enter {book} {chapter}:{verse}")
 
     val addressInput = readLine("address > ")
 
     val pieces = addressInput.split(" ")
-    val bookName = pieces(0)
-    val chapterNumber = pieces(1).split(":")(0)
-    val verseNumber = pieces(1).split(":")(1)
 
-    val verse = this.locate(bookName, chapterNumber, verseNumber)
+    if (pieces.length != 2) {
+      println("format should be {book} {chapter}:{verse}")
+      return
+    }
+
+    val bookName = pieces(0)
+
+    val numbers = pieces(1).split(":")
+
+    if(numbers.length != 2) {
+      println("format should be {book} {chapter}:{verse}")
+      return
+    }
+    val verse = this.locate(bookName, numbers(0), numbers(1))
 
     println(verse)
-
   }
 }
