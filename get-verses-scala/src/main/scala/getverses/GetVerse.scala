cf8df58ad45a813492dc1e139fc52efeeeaac1bc
Emmanuel Nkrumah-Sarpong <nkrumahthis@gmail.com>
2024-05-30 23:19:20 +0000

    make it a loop

--- get-verses-scala/src/main/scala/getverses/GetVerse.scala
+++ get-verses-scala/src/main/scala/getverses/GetVerse.scala
@@ -13,25 +13,27 @@ object GetVerse extends App {
 
   private val bibleObject = ujson.read(bibleString)
 
-  println(
-    """
-      |Welcome to Get-Verse
-      | [1] 1 step quote finder
-      | [2] 3 step quote finder
-      | [3] search for verses
-      |""".stripMargin)
-  private val mode = readLine("> ")
+  while (true) {
+    println(
+      """
+        |Welcome to Get-Verse
+        | [1] 1 step quote finder
+        | [2] 3 step quote finder
+        | [3] search for verses
+        |""".stripMargin)
+    val mode = readLine("> ")
 
-  mode match {
-    case "1" =>
-      println("you chose 1 step quote finder")
-      val locator = new OneStepLocator(bibleObject)
-      locator.executor()
-    case "2" =>
-      println("you chose 3 step quote finder")
-      val locator = new ThreeStepLocator(bibleObject)
-      locator.executor()
-    case "3" =>
-      println("you chose to search for verses")
+    mode match {
+      case "1" =>
+        println("you chose 1 step quote finder")
+        val locator = new OneStepLocator(bibleObject)
+        locator.executor()
+      case "2" =>
+        println("you chose 3 step quote finder")
+        val locator = new ThreeStepLocator(bibleObject)
+        locator.executor()
+      case "3" =>
+        println("you chose to search for verses")
+    }
   }
 }
